e7aaa31a70ae767371e040dc47271a1e2f5d3177
jitender-chaudhary <jitender.chaudhary@cawstudios.com>
2023-03-09 15:52:18 +0530

    api-to-receive-message-to-number

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
 
-# Create your views here.
+import requests
+from django.http import HttpResponse
+from decouple import config
+from twilio.twiml.messaging_response import MessagingResponse
+
+
+def sms_webhook(request):
+    incoming_number = request.POST.get('From')
+    message_body = request.POST.get('Body')
+    
+    twiml_response = MessagingResponse()
+    twiml_response.message("Thank you for your message!")
+    
+    return HttpResponse(str(twiml_response))
--- twiliomanager/urls.py
+++ twiliomanager/urls.py
@@ -18,4 +18,5 @@ from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('webhook/', sms_webhook, name='sms_webhook'),
 ]
