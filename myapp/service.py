a44b656e099a2f62b15b64077e3fd7137610710f
jitender-chaudhary <jitender.chaudhary@cawstudios.com>
2023-03-23 10:24:45 +0530

    feat:added business logic to buy_number

--- myapp/service.py
+++ myapp/service.py
@@ -1,9 +1,33 @@
-from twilio.rest import Client
+import requests
+from django.http import HttpResponse
 from django.conf import settings
 from typing import Tuple
 
 
 class PhoneNumberService:
     def buy_phone_number(self) -> str:
-       pass
+        account_sid = config("TWILIO_ACCOUNT_SID")
+        auth_token = config("TWILIO_AUTH_TOKEN")
+        country_code = config("TWILIO_COUNTRY_CODE")
+        phone_number_type = config("TWILIO_PHONE_NUMBER_TYPE")
+        print(account_sid)
+        available_numbers_url = f'https://api.twilio.com/2010-04-01/Accounts/{account_sid}/AvailablePhoneNumbers/{country_code}/{phone_number_type}.json'
+        response = requests.get(available_numbers_url, auth=(account_sid, auth_token))
+        print(response.status_code)
+        print(response.json())
+        response_json = response.json()
+
+        if 'available_phone_numbers' not in response_json:
+            print(f"No phone numbers available")
+            raise Exception('No available phone numbers')
+        else:
+            phone_number = response_json['available_phone_numbers'][0]['phone_number']
+            print(phone_number)
+            return phone_number
+        # incoming_numbers_url = f'https://api.twilio.com/2010-04-01/Accounts/{account_sid}/IncomingPhoneNumbers.json'
+        # data = {'PhoneNumber': phone_number}
+        # response = requests.post(incoming_numbers_url, auth=(account_sid, auth_token), data=data)
+        # print(response.json())
+        # return Response(response.json())
+
 
